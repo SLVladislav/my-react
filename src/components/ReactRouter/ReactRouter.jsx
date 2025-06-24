337ece151f493b5b44ddf6a6728d4e011e03e988
SLVladislav <bakalavrat.789@gmail.com>
2025-06-24 19:27:53 +0300

    add userserachParams/useLocation/lazy

--- src/components/ReactRouter/ReactRouter.jsx
+++ src/components/ReactRouter/ReactRouter.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react"
-import { Link, Navigate, Outlet, Route, Routes, useNavigate, useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Link, Navigate, Outlet, Route, Routes, useNavigate, useParams, useSearchParams } from "react-router-dom"
 
 export const ReactRouter = () => {
     return (
@@ -104,3 +104,140 @@ const Dashboard = () => {
         <h1>Welcome to your personal account</h1>  
     )
 }
+
+const SerachParams = () => {   //----------Вилучення параметрів
+    const [serachParams] = useSearchParams();
+
+    const name = serachParams.get("name");
+    const color = serachParams.get("color");
+    const maxPrice = serachParams.get("maxPrice");
+
+    return (
+        <div>
+            <p>Name: {name}</p>
+            <p>Color:{color}</p>
+            <p>Maximum price: {maxPrice}</p>
+        </div>
+    );
+} 
+
+const UpdateSingleParams = () => {
+    const [serachParams, setSearchParams] = useSearchParams();
+    const name = serachParams.get("name"); //----збереження назви продукту під час пошуку.
+
+    const updateSearchParams = (key, value) => {
+        //----оновлення ключа
+        const updateParams = new URLSearchParams(serachParams);
+        // ---Оновлюємо певний ключ
+        updateParams.set(key, value);
+        //------Застосовуємо зміни до URL
+        setSearchParams(updateParams);
+    };
+    return (
+        <div>
+            <h1>Products</h1>
+            <input type="text" value={name} onChange={e => updateSearchParams("name", e.target.value)} />
+            <h2>Оновлення одного параметра в URL</h2>
+            <p>Поточні параметри:</p>
+            <pre>{serachParams.toString()}</pre>
+
+            <button onClick={() => updateSearchParams('name', 'snickers')}>
+                Змінити name на snickers
+            </button>
+            <button onClick={() => updateSearchParams('maxPrice', 800)}>
+                Змінити maxPrice на 800
+            </button>
+            <button onClick={() => updateSearchParams('inStock', false)}>
+                Змінити inStock на false
+            </button>
+        </div>
+    );
+}
+
+//----Відстеження змін
+const ChangeUser = () => {
+    const [user, setUser] = useState(null);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const username = searchParams.het("username");
+    
+    useEffect(() => {
+        // Тут виконуємо асинхронну операцію,
+        // наприклад HTTP-запит за інформацією про користувача
+        if (username === "") return;
+
+        FakeAPI.getUser(username).then(setUser);
+    }, [username]);
+
+    const updateSearchParams = (key, value) => {
+        const updateParams = new URLSearchParams(searchParams);
+        updateParams.set(key, value);
+        setSearchParams(updateParams);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = e.currentTarget;
+        updateSearchParams("username", form.elements.username.value);
+        form.reset();
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="username"/>
+                <button type="submit">Search</button>
+            </form>
+            {user && <UserInfo user={user}/>}
+        </>
+    )
+}
+
+// ----- Об'єкт місцезнаходження Хук useLocation
+
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Analytics from "path/to/analytics-service";
+
+const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    Analytics.send(location);
+  }, [location]);
+
+  return <div>...</div>;
+};
+
+
+const Products = () => {
+    const location = useLocation();
+  
+    return (
+      <Link to="/product/h-1" state={location}>
+        Navigate to product h-1
+      </Link>
+    );
+  };
+  
+  const ProductDetails = () => {
+    const location = useLocation();
+    const backLinkHref = location.state ?? "/products";
+  
+    return <Link to={backLinkHref}>Back to products</Link>;
+};
+  
+// -------React.lazy() --відповідає за асинхронне завантаження компонента та React.Suspense --призупиняє його відображення до завершення завантаження.
+import { lazy, Suspense } from "react";
+
+const MyComponent = lazy(() => import("path/to/MyComponent"));
+
+const App = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/some-path" element={<MyComponent />} />
+        {/* Інші маршрути */}
+      </Routes>
+    </Suspense>
+  );
+};
